bd30a060700c5440948666b68c19dba9b80eb0c2
Manav Kant <Kant93907>
2019-03-04 09:12:26 -0800

    Complete OpenCV integration and start camera access

---
+++ OpenCVTest/app/src/main/java/org/pltw/examples/opencvtest/MainActivity.java
@@ -0,0 +1,32 @@
+package org.pltw.examples.opencvtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.core.Mat;
+
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        return null;
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+
+    }
+}
